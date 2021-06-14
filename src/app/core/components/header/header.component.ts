5290164ca1f5ef4c0bc7974ed5e103cc7e4b5432
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-06-14 05:44:29 +0200

    Mobile menu finished

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  menuActive: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  activateMenu(): void {
+    this.menuActive ? (this.menuActive = false) : (this.menuActive = true);
+
+    console.log('menu -->', this.menuActive);
   }
-
 }
